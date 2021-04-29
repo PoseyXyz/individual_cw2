997ae74bd18db1298997940c08dd766edb2c41e9
Posey <adeposi60@gmail.com>
2021-04-29 03:02:03 +0100

    Initialized static file middleware

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const path = require("path")
+const fs = require("fs")
 
 const app = express()
 
@@ -7,6 +9,23 @@ app.use(function(req, res, next){
     console.log(`Request protocol: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
     console.log(`Request ${req.method} made to ${req.url} at ${new Date()}`);
     next();
+})
+
+app.use(function(req, res, next){
+    var filePath = path.join(__dirname, "static", req.url)
+    fs.stat(filePath, function(err, fileInfo){
+        if(err){
+            next();
+            return;
+        }
+        if(fileInfo.isFile()) res.sendFile(filePath)
+        else next()
+    })
+})
+
+app.use(function(req, res){
+    res.status(404)
+    res.send('File not found')
 })
 
 
