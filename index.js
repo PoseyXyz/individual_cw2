cafb960e8f9460985fa4f8df78b3eb8b3d4ff5a2
Posey <adeposi60@gmail.com>
2021-04-29 02:57:31 +0100

    Initialized logger middleware

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express')
 const app = express()
 
 
+app.use(function(req, res, next){
+    console.log(`Request protocol: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    console.log(`Request ${req.method} made to ${req.url} at ${new Date()}`);
+    next();
+})
 
 
 
