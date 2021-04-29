8b48e7af8a194c049b5e36b2b42c772fcc23c431
Posey <adeposi60@gmail.com>
2021-04-29 03:13:50 +0100

    Connected to mongodb

--- routes/api/routes.js
+++ routes/api/routes.js
@@ -1,6 +1,15 @@
 const express = require('express')
 
+const mongodb = require('mongodb')
+const url = 'mongodb+srv://Posey:posey@123@lessoncluster.pckqe.mongodb.net/lessons_database?retryWrites=true&w=majority'
+
 
 const router = express.Router()
 
+
+async function loadLessonInfoCollection(){
+    const client = await mongodb.MongoClient.connect(url, {useNewUrlParser:true, useUnifiedTopology:true});
+    
+    return client.db('lessons_database').collection('lessons_information')
+}
 module.exports = router
